659aad28ba99b2bcc9122ecb804b233f4dbc19f5
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 15:12:12 -0700

    Add files via upload

---
+++ Q10.ts
@@ -0,0 +1,15 @@
+// 10. Adding Comments: Choose two of the programs you've written, and add at least one comment to each . If you don't have anything specific to Write 
+// because your programs are to simple at this point, just add your name and the current data at the top of each program File. Then write one sentence 
+// describing what the program does. 
+
+let a: number = 10
+let b: number = 20
+
+// Addition
+console.log(`${a}+${b} = ${a + b}`);
+// Subtraction
+console.log(`${a}-${b} = ${a - b}`);
+// Multiplication
+console.log(`${a}*${b} = ${a * b}`);
+// Division
+console.log(`${a}/${b} = ${a / b}`);
